f3435ceecfbae0b86b665c593f824e9a8cc413dd
Ahmad Rizgifan Fauzhy <119588164+Hyper-Cosmos@users.noreply.github.com>
2023-11-29 23:35:15 +0700

    FIx Model

--- ML/data.py
+++ ML/data.py
@@ -4,10 +4,23 @@ import pandas as pd
 file_path1 = 'inflasibulanfix.csv'
 
 # Membaca file CSV pertama
-df1 = pd.read_csv(file_path1)
+df = pd.read_csv(file_path1)
 
-# Menampilkan isi DataFrame pertama
-print("Data Frame 1:")
-print(df1)
+# Melelehkan kolom bulan-tahun menjadi dua kolom terpisah
+df_melted = pd.melt(df, id_vars=['Kota'], var_name='Month_Year', value_name='Inflation')
+
+# Memisahkan kolom "Month_Year" menjadi dua kolom terpisah
+df_melted[['Month', 'Year']] = df_melted['Month_Year'].str.split('_', expand=True)
+
+# Menampilkan kolom "Kota," "Month," "Year," dan "Inflation"
+df_result = df_melted.loc[:, ['Kota', 'Month', 'Year', 'Inflation']]
+
+# Mengganti nama kolom 'Kota' menjadi 'City'
+df_result = df_result.rename(columns={'Kota': 'City'})
+
+# Menampilkan hasil
+print(df_result)
 
 
+# Menyimpan DataFrame ke dalam file CSV
+df_result.to_csv('output.csv', index=False)
--- ML/mainml.py
+++ ML/mainml.py
@@ -1,48 +1,65 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Bidirectional, LSTM, Dense
 
-# Load your dataset
-file_path = 'inflasibulanfix.csv'
+# Baca data
+file_path = 'output.csv'
 df = pd.read_csv(file_path)
 
-# Convert string numbers with commas to floats
-numeric_columns = df.columns[1:]
-df[numeric_columns] = df[numeric_columns].replace(',', '.', regex=True).astype(float)
+# Ambil kolom yang relevan
+df = df[['City', 'Month', 'Year', 'Inflation']]
 
-# Normalize data
+# Ganti koma dengan titik pada kolom 'Inflation'
+df['Inflation'] = df['Inflation'].str.replace(',', '.').astype(float)
+
+# Urutkan data
+df = df.sort_values(by=['City', 'Year', 'Month'])
+
+# Scaling data inflasi
 scaler = MinMaxScaler()
-normalized_data = scaler.fit_transform(df[numeric_columns])
+df['Inflation'] = scaler.fit_transform(df['Inflation'].values.reshape(-1, 1))
 
-# Prepare the input data
-X = normalized_data[:, :-1]
-y = normalized_data[:, -1]
+# Pisahkan data menjadi seri waktu untuk setiap kota
+city_data = {}
+for city in df['City'].unique():
+    city_data[city] = df[df['City'] == city]['Inflation'].values
 
-# Reshape data for LSTM
-X = X.reshape((X.shape[0], 1, X.shape[1]))
+# Fungsi untuk membuat sekuens waktu
+def create_time_series(data, time_steps=1):
+    X, y = [], []
+    for i in range(len(data) - time_steps):
+        a = data[i:(i + time_steps)]
+        X.append(a)
+        y.append(data[i + time_steps])
+    return np.array(X), np.array(y)
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Hyperparameter
+time_steps = 12  # Sesuaikan dengan panjang sekuens waktu yang ingin Anda gunakan
+n_features = 1  # Jumlah fitur (hanya inflasi)
 
-# Build the Bidirectional LSTM model
-model = Sequential()
-model.add(Bidirectional(LSTM(50, activation='relu'), input_shape=(X.shape[1], X.shape[2])))
-model.add(Dense(1))
-model.compile(optimizer='adam', loss='mse')
+# Membuat dan melatih model LSTM untuk setiap kota
+models = {}
+for city, data in city_data.items():
+    X, y = create_time_series(data, time_steps)
+    X = X.reshape((X.shape[0], X.shape[1], n_features))
+    
+    model = Sequential()
+    model.add(Bidirectional(LSTM(50, activation='relu'), input_shape=(time_steps, n_features)))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mse')
+    model.fit(X, y, epochs=50, verbose=0)
+    
+    models[city] = model
 
-# Train the model
-model.fit(X_train, y_train, epochs=100, validation_data=(X_test, y_test))
+# Contoh prediksi untuk suatu kota
+sample_city = 'KOTA BANDA ACEH'
+input_data = scaler.transform(city_data[sample_city][-time_steps:].reshape(-1, 1))
+input_data = input_data.reshape((1, time_steps, n_features))
+predicted_inflation = models[sample_city].predict(input_data)
 
-# Evaluate the model on the testing data
-mse = model.evaluate(X_test, y_test)
-print(f'Mean Squared Error on Testing Data: {mse}')
+# Inverse transform hasil prediksi untuk mendapatkan nilai sebenarnya
+predicted_inflation_actual = scaler.inverse_transform(predicted_inflation.reshape(-1, 1))
 
-# # Make predictions on new data (adjust as needed)
-# new_data = np.array([[0.1, 0.2, 0.3, 0.4]])  # Replace with your actual data
-# new_data = new_data.reshape((new_data.shape[0], 1, new_data.shape[1]))
-# scaled_new_data = scaler.transform(new_data)
-# predicted_inflation = model.predict(scaled_new_data)
-# print(f'Predicted Inflation: {predicted_inflation}')
+print(f'Predicted Inflation for {sample_city}: {predicted_inflation_actual[0][0]}')
