add2851a9e1c478ec2017bbafdd6e344defde520
Ahmad Rizgifan Fauzhy <119588164+Hyper-Cosmos@users.noreply.github.com>
2023-11-29 14:20:45 +0700

    Model Fix

--- ML/mainml.py
+++ ML/mainml.py
@@ -1,51 +1,48 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Bidirectional, LSTM, Dense
 
-# Baca dataset
+# Load your dataset
 file_path = 'inflasibulanfix.csv'
 df = pd.read_csv(file_path)
 
-# Pilih kota yang ingin diprediksi
-selected_city = 'KOTA YOGYAKARTA'
-city_data = df[df['Kota'] == selected_city]
+# Convert string numbers with commas to floats
+numeric_columns = df.columns[1:]
+df[numeric_columns] = df[numeric_columns].replace(',', '.', regex=True).astype(float)
 
-# Pilih kolom inflasi, pendapatan, dan pengeluaran
-selected_columns = ['januari_2008', 'februari_2008', 'maret_2008', 'april_2008', 'mei_2008']  # Sesuaikan dengan nama kolom Anda
-data = city_data[selected_columns].values
-
-# Normalisasi data
+# Normalize data
 scaler = MinMaxScaler()
-scaled_data = scaler.fit_transform(data)
+normalized_data = scaler.fit_transform(df[numeric_columns])
 
-# Reshape data untuk LSTM
-X_train = scaled_data[:, :-1].reshape((scaled_data.shape[0], 1, scaled_data.shape[1] - 1))
-y_train = scaled_data[:, -1]
+# Prepare the input data
+X = normalized_data[:, :-1]
+y = normalized_data[:, -1]
 
-# Buat model LSTM
+# Reshape data for LSTM
+X = X.reshape((X.shape[0], 1, X.shape[1]))
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Build the Bidirectional LSTM model
 model = Sequential()
-model.add(Bidirectional(LSTM(50, activation='relu'), input_shape=(X_train.shape[1], X_train.shape[2])))
+model.add(Bidirectional(LSTM(50, activation='relu'), input_shape=(X.shape[1], X.shape[2])))
 model.add(Dense(1))
 model.compile(optimizer='adam', loss='mse')
 
-# Latih model
-model.fit(X_train, y_train, epochs=100)
+# Train the model
+model.fit(X_train, y_train, epochs=100, validation_data=(X_test, y_test))
 
-# Evaluate the model on the training data
-mse = model.evaluate(X_train, y_train)
-print(f'Mean Squared Error on Training Data: {mse}')
+# Evaluate the model on the testing data
+mse = model.evaluate(X_test, y_test)
+print(f'Mean Squared Error on Testing Data: {mse}')
 
-
-# # Prediksi masa depan
-# # Assuming your future data has 4 features and you want to add a placeholder for the missing feature
-# future_data = np.array([[0.1, 0.2, 0.3, 0.4]])  # Replace with your actual data
-# future_data = np.hstack((future_data, np.zeros((future_data.shape[0], 1))))  # Add a placeholder for the missing feature
-# scaled_future_data = scaler.transform(future_data)
-# scaled_future_data = scaled_future_data.reshape((1, 1, scaled_future_data.shape[1]))
-# predicted_inflation = model.predict(scaled_future_data)
+# # Make predictions on new data (adjust as needed)
+# new_data = np.array([[0.1, 0.2, 0.3, 0.4]])  # Replace with your actual data
+# new_data = new_data.reshape((new_data.shape[0], 1, new_data.shape[1]))
+# scaled_new_data = scaler.transform(new_data)
+# predicted_inflation = model.predict(scaled_new_data)
 # print(f'Predicted Inflation: {predicted_inflation}')
-
-
-
