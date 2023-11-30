98ab9e2cbdac20f4cf8656645ceb29d77bf9d741
LaluBRamadhan <lalubagoes2@gmail.com>
2023-11-30 14:17:16 +0700

    intent bug fixed

--- MD/app/src/main/java/com/code/precapstone/ui/view/login/LoginActivity.kt
+++ MD/app/src/main/java/com/code/precapstone/ui/view/login/LoginActivity.kt
@@ -1,12 +1,24 @@
 package com.code.precapstone.ui.view.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.code.precapstone.R
+import com.code.precapstone.databinding.ActivityLoginBinding
+import com.code.precapstone.ui.view.signup.SignUpActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val tvRegister = binding.textRegister
+        tvRegister.setOnClickListener{
+            val intent = Intent(applicationContext, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- MD/app/src/main/java/com/code/precapstone/ui/view/signup/SignUpActivity.kt
+++ MD/app/src/main/java/com/code/precapstone/ui/view/signup/SignUpActivity.kt
@@ -1,12 +1,28 @@
 package com.code.precapstone.ui.view.signup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.code.precapstone.R
+import com.code.precapstone.databinding.ActivitySignUpBinding
+import com.code.precapstone.ui.view.login.LoginActivity
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            textLogin.setOnClickListener{
+                val intent = Intent(applicationContext, LoginActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
