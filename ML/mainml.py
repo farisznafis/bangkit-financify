a5900a1d469e02404a1d49557aae83d9e4f13e1c
Ahmad Rizgifan Fauzhy <119588164+Hyper-Cosmos@users.noreply.github.com>
2023-12-05 20:51:44 +0700

    Updated LSTM Model

--- ML/mainml.py
+++ ML/mainml.py
@@ -3,30 +3,31 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Bidirectional, LSTM, Dense
+import tensorflow as tf
 
-# Baca data
+# Load data
 file_path = 'inflasibulanfix.csv'
 df = pd.read_csv(file_path)
 
-# Ambil kolom yang relevan
+# Select relevant columns
 df = df[['City', 'Month', 'Year', 'Inflation']]
 
-# Ganti koma dengan titik pada kolom 'Inflation'
+# Replace commas with dots in the 'Inflation' column
 df['Inflation'] = df['Inflation'].str.replace(',', '.').astype(float)
 
-# Urutkan data
+# Sort the data
 df = df.sort_values(by=['City', 'Year', 'Month'])
 
-# Scaling data inflasi
+# Scale inflation data
 scaler = MinMaxScaler()
 df['Inflation'] = scaler.fit_transform(df['Inflation'].values.reshape(-1, 1))
 
-# Pisahkan data menjadi seri waktu untuk setiap kota
+# Separate data into time series for each city
 city_data = {}
 for city in df['City'].unique():
     city_data[city] = df[df['City'] == city]['Inflation'].values
 
-# Fungsi untuk membuat sekuens waktu
+# Function to create time series sequences
 def create_time_series(data, time_steps=1):
     X, y = [], []
     for i in range(len(data) - time_steps):
@@ -35,11 +36,11 @@ def create_time_series(data, time_steps=1):
         y.append(data[i + time_steps])
     return np.array(X), np.array(y)
 
-# Hyperparameter
-time_steps = 12  # Sesuaikan dengan panjang sekuens waktu yang ingin Anda gunakan
-n_features = 1  # Jumlah fitur (hanya inflasi)
+# Hyperparameters
+time_steps = 12
+n_features = 1
 
-# Membuat dan melatih model LSTM untuk setiap kota
+# Create and train LSTM model for each city
 models = {}
 for city, data in city_data.items():
     X, y = create_time_series(data, time_steps)
@@ -53,13 +54,29 @@ for city, data in city_data.items():
     
     models[city] = model
 
-# Contoh prediksi untuk suatu kota
+# Example prediction for a city
 sample_city = 'KOTA YOGYAKARTA'
 input_data = scaler.transform(city_data[sample_city][-time_steps:].reshape(-1, 1))
 input_data = input_data.reshape((1, time_steps, n_features))
 predicted_inflation = models[sample_city].predict(input_data)
 
-# Inverse transform hasil prediksi untuk mendapatkan nilai sebenarnya
+# Inverse transform the prediction to get the actual value
 predicted_inflation_actual = scaler.inverse_transform(predicted_inflation.reshape(-1, 1))
-
 print(f'Predicted Inflation for {sample_city}: {predicted_inflation_actual[0][0]}')
+
+# Compare with the most recent actual inflation value
+historical_inflation = city_data[sample_city]
+last_actual_inflation = historical_inflation[-1]
+
+# Determine the trend
+if predicted_inflation_actual[0][0] > last_actual_inflation:
+    trend = 'up'
+elif predicted_inflation_actual[0][0] < last_actual_inflation:
+    trend = 'down'
+else:
+    trend = 'unchanged'
+
+print(f'Trend for {sample_city} inflation: {trend}')
+
+# Save the Keras model in the native format
+models[sample_city].save('lstm_model.h5')
