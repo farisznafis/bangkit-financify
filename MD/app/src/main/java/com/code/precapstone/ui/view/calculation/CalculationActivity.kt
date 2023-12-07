165e272509473126a6022f683436f14e3bd4bab9
Charlos Matio Simanjuntak <211111734@students.mikroskil.ac.id>
2023-12-07 13:05:02 +0700

    Menambahkan layout CalculationActivity

---
+++ MD/app/src/main/java/com/code/precapstone/ui/view/calculation/CalculationActivity.kt
@@ -0,0 +1,12 @@
+package com.code.precapstone.ui.view.calculation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.code.precapstone.R
+
+class CalculationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculation)
+    }
+}
