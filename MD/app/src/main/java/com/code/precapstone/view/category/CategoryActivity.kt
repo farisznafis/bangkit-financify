400af11a500c7862940dfef67dcdef3a4ffcbb2e
LaluBRamadhan <lalubagoes2@gmail.com>
2023-12-13 22:54:03 +0700

    connecting somepage

--- MD/app/src/main/java/com/code/precapstone/view/category/CategoryActivity.kt
+++ MD/app/src/main/java/com/code/precapstone/view/category/CategoryActivity.kt
@@ -1,12 +1,26 @@
 package com.code.precapstone.view.category
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.code.precapstone.R
+import com.code.precapstone.databinding.ActivityCategoryBinding
+import com.code.precapstone.databinding.ActivitySettingsBinding
+import com.code.precapstone.view.login.LoginActivity
+import com.code.precapstone.view.main.MainActivity
 
 class CategoryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCategoryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_category)
+        binding = ActivityCategoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.backIcon.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- MD/app/src/main/java/com/code/precapstone/view/main/MainActivity.kt
+++ MD/app/src/main/java/com/code/precapstone/view/main/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.code.precapstone.R
 import com.code.precapstone.databinding.ActivityMainBinding
+import com.code.precapstone.view.category.CategoryActivity
 import com.code.precapstone.view.setting.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,11 @@ class MainActivity : AppCompatActivity() {
 
         binding.ivSetting.setOnClickListener{
             val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.fab.setOnClickListener{
+            val intent = Intent(this, CategoryActivity::class.java)
             startActivity(intent)
         }
 
