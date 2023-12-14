bab45342ee99b4f8b6e5735a4163648993c53522
Charlos Matio Simanjuntak <211111734@students.mikroskil.ac.id>
2023-12-14 14:29:23 +0700

    Add Article Activity
    Menambahkan Layout Article Activity, serta menambah beberapa jenis font dan drawable

---
+++ MD/app/src/main/java/com/code/precapstone/view/articles/ArticleActivity.kt
@@ -0,0 +1,12 @@
+package com.code.precapstone.view.articles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.code.precapstone.R
+
+class ArticleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article)
+    }
+}
