2e96e2a3a2251b79acabda307f27317105625266
LaluBRamadhan <lalubagoes2@gmail.com>
2023-12-16 19:02:53 +0700

    creating detail activity
    wleowleo

---
+++ MD/app/src/main/java/com/code/precapstone/view/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.code.precapstone.view.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.code.precapstone.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
